8b553650a6e0fc3e7d0a7693734dd399c6a0d8db
Christos Tranoris <tranoris@gmail.com>
2020-08-26 20:48:13 +0300

    tedt

--- web_interface/app.py
+++ web_interface/app.py
@@ -419,6 +419,63 @@ def tts():
 		print("Play TTS clip:", clip)
 		pygame.mixer.music.load(clip)
 		pygame.mixer.music.play()	
+		
+		"""
+	    Program to make a robot voice by simulating a ring modulator;
+	    procedure/math taken from
+	    http://recherche.ircam.fr/pub/dafx11/Papers/66_e.pdf
+	    """
+		rate, data = readmp3(clip) #wavfile.read(clip)
+		data = data[:,1]
+	
+		# get max value to scale to original volume at the end
+		scaler = np.max(np.abs(data))
+	
+		# Normalize to floats in range -1.0 < data < 1.0
+		data = data.astype(np.float)/scaler
+	
+		# Length of array (number of samples)
+		n_samples = data.shape[0]
+	
+		# Create the lookup table for simulating the diode.
+		d_lookup = diode_lookup(LOOKUP_SAMPLES)
+		diode = Waveshaper(d_lookup)
+	
+		# Simulate sine wave of frequency MOD_F (in Hz)
+		tone = np.arange(n_samples)
+		tone = np.sin(2*np.pi*tone*MOD_F/rate)
+	
+		# Gain tone by 1/2
+		tone = tone * 0.5
+	
+		# Junctions here
+		tone2 = tone.copy() # to top path
+		data2 = data.copy() # to bottom path
+	
+		# Invert tone, sum paths
+		tone = -tone + data2 # bottom path
+		data = data + tone2 #top path
+	
+		#top
+		data = diode.transform(data) + diode.transform(-data)
+	
+		#bottom
+		tone = diode.transform(tone) + diode.transform(-tone)
+	
+		result = data - tone
+	
+		#scale to +-1.0
+		result /= np.max(np.abs(result))
+		#now scale to max value of input file.
+		result *= scaler
+		# wavfile.write wants ints between +-5000; hence the cast
+		#wavfile.write(clip, rate, result.astype(np.int16))
+		writemp3(clip, rate, result.astype(np.int16))
+	    
+	    
+		print("Play ROBOT music clip:", clip)
+		pygame.mixer.music.load(clip)
+		pygame.mixer.music.play()
 		return jsonify({'status': 'OK' })
 	else:
 		return jsonify({'status': 'Error','msg':'Unable to read POST data'})
